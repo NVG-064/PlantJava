6da02cc7efd8681255e8c1dadad7bd9884ed6b3b
ink0rr <tomflynnbeltsazar@gmail.com>
2021-12-13 22:42:59 +0700

    Refactor plant class

--- src/Plant.java
+++ src/Plant.java
@@ -1,57 +1,84 @@
-public class Plant {
-    int statusTumbuh; // 0-4
-    int jumlahAir;
-    int jumlahPupuk;
+public abstract class Plant {
+    protected int growthStage; // 0-4
+    protected String plantName;
 
-    public void beriAir() {
-        jumlahAir++;
-        cekKondisiTumbuh();
+    protected int waterCount;
+    protected int fertilizerCount;
+
+    public Plant() { // Constructor
+        init();
     }
 
-    public void beriPupuk() {
-        jumlahPupuk++;
-        cekKondisiTumbuh();
+    protected void init() {
+        growthStage = 0;
+        waterCount = 0;
+        fertilizerCount = 0;
     }
 
-    public void cekKondisiTumbuh() {
+    public String getPlantName() {
+        return plantName;
+    }
+
+    public int getGrowthStatus() {
+        return growthStage;
+    }
+
+    public String displayGrowthStatus() {
+        switch (growthStage) {
+            case 0:
+                return "Benih";
+            case 1:
+                return "Tunas";
+            case 2:
+                return "Tanaman Kecil";
+            case 3:
+                return "Tanaman Dewasa";
+            case 4:
+                return "Berbunga";
+        }
+        return "Unknown";
+    }
+
+    public void giveWater() {
+        waterCount++;
+        checkGrowth();
+    }
+
+    public void giveFertilizer() {
+        fertilizerCount++;
+        checkGrowth();
+    }
+
+    public void checkGrowth() {
         // cek kecukupan air dan pupuk
-        if ((jumlahAir >= 3) && (jumlahPupuk >= 1)) tumbuh(); // trigger tumbuh()
+        if ((waterCount >= 3) && (fertilizerCount >= 1))
+            grow(); // trigger grow()
     }
 
-    public void tumbuh() {
-        if (statusTumbuh < 4) {
-            jumlahAir -= 3; // Same as jumlahAir = jumlahAir - 3
-            jumlahPupuk -= 1; // Same as jumlahPupuk = jumlahPupuk - 1
-            statusTumbuh++;
+    public void grow() {
+        if (growthStage < 4) {
+            waterCount -= 3; // Same as jumlahAir = jumlahAir - 3
+            fertilizerCount -= 1; // Same as jumlahPupuk = jumlahPupuk - 1
+            growthStage++;
         }
     }
 
     public void displayPlant() {
-        System.out.println(getStatusTumbuhText() + " (" + getStatusTumbuh() + ")\n");
-        System.out.println("Jumlah air      : " + jumlahAir);
-        System.out.println("Jumlah pupuk    : " + jumlahPupuk);
+        System.out.println(displayGrowthStatus() + " (Tahap " + getGrowthStatus() + ")\n");
+        if (growthStage > 3)
+            System.out.println("Nama Tanaman    : " + getPlantName() + "\n");
+        System.out.println("Jumlah air      : " + waterCount);
+        System.out.println("Jumlah pupuk    : " + fertilizerCount);
     }
 
-    public String getStatusTumbuhText() {
-        switch(statusTumbuh) {
-            case 0: return "Benih"; // 0
-            case 1: return "Tunas"; // 1
-            case 2: return "Tanaman Kecil"; // 2
-            case 3: return "Tanaman Dewasa"; // 3
+    public void resetPlant() {
+        init();
+        MyUtils.clearScreen();
+        System.out.print("Tanaman ditebang dan benih baru ditanam");
+        try {
+            Thread.sleep(200);
+        } catch (Exception e) {
+
         }
-
-        return "Berbunga"; // 4
-    }
-
-    public int getStatusTumbuh() {
-        return statusTumbuh;
-    }
-
-    // getImagePath tidak dimasukkan karena ini adalah CLI
-
-    public Plant() { // Constructor
-        statusTumbuh = 0;
-        jumlahAir = 0;
-        jumlahPupuk = 0;
     }
 }
--- src/PlantMain.java
+++ src/PlantMain.java
@@ -1,115 +1,52 @@
 import java.util.Scanner; // Import Scanner library
 
 public class PlantMain {
-    static Scanner sc = new Scanner(System.in); // Membuat scanner baru bernama sc
-    public static void main(String[] myString) {
-        int hasilRandom = -1;
-        int exceptionFlag = 0; // Anggaplah program berjalan dengan benar pada saat pertama kali dijalankan
+
+    private static Plant plant;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        initPlant();
+
         for (;;) {
-            if (hasilRandom == -1) {
-                RandomNumber rn = new RandomNumber();
-                hasilRandom = rn.createRandomNumber(0, 1);
+            MyUtils.clearScreen(); // Clear screen
+            System.out.println("\t\tMiGarden\n");
+
+            plant.displayPlant(); // Display Plant
+            System.out.println("\nMasukkan:");
+            System.out.println("1 untuk memberi air");
+            System.out.println("2 untuk memberi pupuk");
+            System.out.println("5 untuk tebang tanaman dan tanam benih baru");
+            System.out.println("7 untuk keluar dari program\n");
+
+            int input = sc.nextInt(); // Scan user input
+            switch (input) {
+                case 1:
+                    plant.giveWater(); // Tambah air
+                    break;
+
+                case 2:
+                    plant.giveFertilizer(); // Tambah pupuk
+                    break;
+
+                case 5:
+                    initPlant();
+                    break;
+
+                case 7:
+                    sc.close();
+                    System.exit(0); // Exit the program
+                    break;
             }
-
-            if (hasilRandom == 0) {
-                Fruit fruit = new Fruit(); // Create new Fruit Class called p
-
-                for (;;) {
-                    try {
-                        if (exceptionFlag == 1) {
-                            showExceptionText(exceptionFlag);
-                            Thread.sleep(2000); // Pause system to 2 secs
-                        } // If
-        
-                        showMainPage();;
-                        fruit.displayPlant(); // Display Plant
-                        System.out.println("\n\nMasukkan:\n0 untuk memberi air\n1 untuk memberi pupuk\n5 untuk tebang tanaman dan tanam benih baru\n7 untuk keluar dari program\n");
-                        int input = sc.nextInt(); // Scan user input
-                        switch(input) {
-                            case 0:
-                                fruit.beriAir(); // Add Air
-                                break;
-                            
-                            case 1:
-                                fruit.beriPupuk(); // Add Pupuk
-                                break;
-        
-                            case 5:
-                                fruit.resetTanam(); // Reset program
-                                Thread.sleep(2000); // Pause system to 2 secs
-                                main(myString);
-                                break;
-                            
-                            case 7:
-                                System.exit(0); // Equals to return 0 in C++
-                                break;
-                        } // Switch
-        
-                        exceptionFlag = 0; // Return back to 0 to avoid message printing
-        
-                        // Try
-                    } catch (Exception nonIntegerInput) { // Action if we wrong input program
-                        sc.nextLine(); // Clear the Scanner
-                        exceptionFlag = 1; // Trigger the flag to show Exception messages
-                    } // Catch
-                }
-            } // If
-
-            else if (hasilRandom == 1) {
-                Flower flower = new Flower();
-
-                for (;;) {
-                    try {
-                        if (exceptionFlag == 1) {
-                            showExceptionText(exceptionFlag);
-                            Thread.sleep(2000); // Pause system to 2 secs
-                        } // If
-        
-                        showMainPage();
-                        flower.displayPlant(); // Display Plant
-                        System.out.println("\n\nMasukkan:\n0 untuk memberi air\n1 untuk memberi pupuk\n5 untuk tebang tanaman dan tanam benih baru\n7 untuk keluar dari program\n");
-                        int input = sc.nextInt(); // Scan user input
-                        switch(input) {
-                            case 0:
-                                flower.beriAir(); // Add Air
-                                break;
-                            
-                            case 1:
-                                flower.beriPupuk(); // Add Pupuk
-                                break;
-        
-                            case 5:
-                                flower.resetTanam(); // Reset program
-                                Thread.sleep(2000); // Pause system to 2 secs
-                                main(myString);
-                                break;
-                            
-                            case 7:
-                                System.exit(0); // Equals to return 0 in C++
-                                break;
-                        } // Switch
-        
-                        exceptionFlag = 0; // Return back to 0 to avoid message printing
-        
-                        // Try
-                    } catch (Exception nonIntegerInput) { // Action if we wrong input program
-                        sc.nextLine(); // Clear the Scanner
-                        exceptionFlag = 1; // Trigger the flag to show Exception messages
-                    } // Catch
-                }
-            } // Else If
-        } // For
-    } // Main()
-
-    static void showMainPage() {
-        System.out.print("\033[H\033[2J"); // Clear screen
-        System.out.println("\t\tMiGarden\n");
-    }
-
-    static void showExceptionText(int flags) {
-        if (flags == 1) {
-            System.out.print("\033[H\033[2J"); // Clear screen
-            System.out.print("Wrong input (non integer) found. Try to input again."); // Exception messages goes here
         }
     }
-} // Class PlantMain
+
+    static void initPlant() {
+        RandomNumber rng = new RandomNumber();
+        int num = rng.createRandomNumber(0, 1);
+        if (num == 0)
+            plant = new Fruit();
+        else
+            plant = new Flower();
+    }
+}
